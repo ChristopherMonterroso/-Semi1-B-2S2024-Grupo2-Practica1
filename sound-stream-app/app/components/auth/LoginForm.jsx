367b87bcf243a0a48cb7c9394ca622447f845475
Estuardo Ruiz <zruiz89@gmail.com>
2024-08-24 16:18:40 -0600

    Agregada pagina de login con estilos

--- sound-stream-app/app/components/auth/LoginForm.jsx
+++ sound-stream-app/app/components/auth/LoginForm.jsx
@@ -2,35 +2,36 @@
 
 import { useState } from 'react';
 import styles from './LoginForm.module.css';
+import Logo from './Logo';
+import SignupLink from './SignupLink';
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [submitted, setSubmitted] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setSubmitted(true);
-    console.log('Username:', username);
-    console.log('Password:', password);
+    console.log("Login");
   };
 
   return (
-    <div className={styles.formContainer}>
+    <div className={styles.formWrapper}>
+      <Logo />
+
       <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.formGroup}>
-          <label className={styles.label} htmlFor="username">Username</label>
           <input
             className={styles.input}
             type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
+            placeholder='Correo electrónico'
           />
         </div>
+
         <div className={styles.formGroup}>
-          <label className={styles.label} htmlFor="password">Password</label>
           <input
             className={styles.input}
             type="password"
@@ -38,12 +39,17 @@ const LoginForm = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
+            placeholder='Contraseña'
           />
         </div>
+        
         <button type="submit" className={styles.button}>
-          Login
+          Iniciar sesión
         </button>
-        {submitted && <p className={styles.submittedMessage}>Form submitted!</p>}
+
+        {/*submitted && <p className={styles.submittedMessage}>Form submitted!</p>*/}
+        
+        <SignupLink />
       </form>
     </div>
   );
--- sound-stream-app/pages/login.js
+++ sound-stream-app/pages/login.js
@@ -1,10 +1,14 @@
-import LoginForm from "../app/components/auth/LoginForm"
+import LoginForm from '../app/components/auth/LoginForm';
+import styles from '../styles/LoginPage.module.css';
+import '../app/globals.css'
 
-export default function Home() {
+export default function LoginPage() {
   return (
-    <main>
-      <LoginForm />
+    <main className={styles.container}>
+      <div className={styles.imageContainer}></div>
+      <div className={styles.formContainer}>
+        <LoginForm />
+      </div>
     </main>
   );
 }
- 
